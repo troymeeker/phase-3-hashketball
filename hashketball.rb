603579ff21ef4364de814a11e6196e4c0574c991
troymeeker <troymeeker1@gmail.com>
2021-12-16 18:46:06 -0800

    commit tree 60e2b040981d04754ab91b0a9485831f482ff8e1

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
 # Write your code below game_hash
+
 def game_hash
   {
     home: {
@@ -127,3 +128,112 @@ def game_hash
 end
 
 # Write code here
+
+
+
+def num_points_scored provided_p_name
+  provided_p_points = 0
+
+  game_hash[:home][:players].select do |player|
+    if player[:player_name] == provided_p_name
+      puts player[:points].to_i
+      provided_p_points = player[:points]
+    end
+  end
+
+  game_hash[:away][:players].select do |player|
+    if player[:player_name] == provided_p_name
+      puts player[:points].to_i
+      provided_p_points = player[:points]
+    end
+  end
+provided_p_points
+end
+
+def shoe_size provided_p_name
+  provided_p_shoe = 0
+  game_hash[:home][:players].select do |player|
+    if player[:player_name] == provided_p_name
+      puts player[:shoe]
+      provided_p_shoe = player[:shoe]
+    end
+  end
+
+  game_hash[:away][:players].select do |player|
+    if player[:player_name] == provided_p_name
+      puts player[:shoe]
+      provided_p_shoe = player[:shoe]
+    end
+  end
+  provided_p_shoe
+end
+
+def team_colors provided_team
+ 
+  if game_hash[:home][:team_name] == provided_team
+     game_hash[:home][:colors]
+  elsif game_hash[:away][:team_name] == provided_team
+     game_hash[:away][:colors]
+  end
+end
+
+
+def team_names 
+  all_teams = []
+  all_teams.push(game_hash[:home][:team_name], game_hash[:away][:team_name])
+  all_teams
+
+end
+
+def player_numbers team_name
+  nums = []
+  if game_hash[:home][:team_name] == team_name 
+   game_hash[:home][:players].each do |player|
+    nums.push(player[:number])
+   end
+
+  elsif  game_hash[:away][:team_name] == team_name 
+   game_hash[:away][:players].each do |player|
+   nums.push(player[:number] )
+   end
+  end
+  nums
+
+end
+
+def player_stats given_player_name
+  stat_hash = {}
+    game_hash[:home][:players].each do |player|
+      if given_player_name == player[:player_name] 
+        stat_hash = player
+      end
+    end
+    
+
+    game_hash[:away][:players].each do |player|
+      if given_player_name == player[:player_name]
+        stat_hash = player
+      end
+    end
+    stat_hash
+end 
+
+def big_shoe_rebounds
+  biggest_shoes = 0
+  player_with_biggest_shoes = {}
+
+  game_hash[:home][:players].each do |player|
+    if player[:shoe] > biggest_shoes
+      biggest_shoes = player[:shoe]
+      player_with_biggest_shoes = player
+    end 
+  end
+  game_hash[:away][:players].each do |player|
+    if player[:shoe] > biggest_shoes
+      biggest_shoes = player[:shoe]
+       player_with_biggest_shoes = player
+     
+    end
+  end
+   player_with_biggest_shoes[:rebounds]
+end
